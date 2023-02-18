1978f502e8b6add0d0bc6ca9b12a03271e5f59d1
Alex Mandebura <xenovidbis@gmail.com>
2023-02-18 16:07:25 +0200

    upd libs

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import { RecipeList } from './RecipeList';
+import recipes from '../recipes.json';
+export const App = () => {
+  return (
+    <>
+      <RecipeList recipes={recipes} />
+    </>
+  );
+};
---
+++ src/components/Recipe.js
@@ -0,0 +1,46 @@
+import { PropTypes } from 'prop-types';
+import { BsAlarm } from 'react-icons/bs';
+import { GiFat } from 'react-icons/gi';
+
+import { MdOutlineFastfood } from 'react-icons/md';
+export const Recipe = ({
+  recipe: { name, image, calories, time, servings },
+}) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={image} alt={name} width="240"></img>
+      <div>
+        <p>
+          <BsAlarm /> {time} min
+        </p>
+        <p>
+          <MdOutlineFastfood /> {servings} servings
+        </p>
+        <p>
+          <GiFat /> {calories} calories
+        </p>
+        <div>
+          <h3>Difficulty</h3>
+        </div>
+        <div>
+          <span>Easy</span>
+          <span>Medium</span>
+          <span>Hard</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Recipe.propTypes = {
+  recipe: PropTypes.exact({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    calories: PropTypes.number.isRequired,
+    time: PropTypes.string.isRequired,
+    servings: PropTypes.number.isRequired,
+    difficulty: PropTypes.oneOf(['easy', 'medium', 'hard']).isRequired,
+  }).isRequired,
+};
